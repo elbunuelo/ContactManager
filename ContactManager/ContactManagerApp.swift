0218674363fa34a3c7890793d396d10d6b0e55f8
Nicolas Arias <elbunuelo@gmail.com>
2020-06-27 10:48:21 -0500

    Initial Commit

---
+++ ContactManager/ContactManagerApp.swift
@@ -0,0 +1,17 @@
+//
+//  ContactManagerApp.swift
+//  ContactManager
+//
+//  Created by Nicolas Arias on 6/27/20.
+//
+
+import SwiftUI
+
+@main
+struct ContactManagerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
